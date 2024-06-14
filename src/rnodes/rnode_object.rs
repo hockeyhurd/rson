7e2a2f63af5f565ab66b45d0786f0f591a301251
hockeyhurd <hockeyhurd@users.noreply.github.com>
2024-06-14 16:43:04 -0400

    Issue #12: Implement builder pattern. (#15)
    * Feat(RNodeObject): Implement builder pattern.
    
    * Add simple test using the RNodeObject's new builder pattern.

--- src/rnodes/rnode_object.rs
+++ src/rnodes/rnode_object.rs
@@ -12,7 +12,6 @@ pub struct RNodeObject
 
 impl RNodeObject
 {
-    #[allow(dead_code)]
     pub fn new(map: BTreeMap<String, Rc<dyn RNode>>) -> Self
     {
         Self { map }
@@ -28,6 +27,20 @@ impl RNodeObject
     pub fn len(&self) -> usize
     {
         return self.map.len();
+    }
+
+    #[allow(dead_code)]
+    pub fn add_copy(mut self, key: &String, value: Rc<dyn RNode>) -> Self
+    {
+        self.map.insert(key.clone(), value);
+        self
+    }
+
+    #[allow(dead_code)]
+    pub fn add_move(mut self, key: String, value: Rc<dyn RNode>) -> Self
+    {
+        self.map.insert(key, value);
+        self
     }
 
     #[allow(dead_code)]
@@ -80,6 +93,39 @@ impl Deref for RNodeObject
     fn deref(&self) -> &Self::Target
     {
         return &self.map;
+    }
+}
+
+#[cfg(test)]
+mod tests
+{
+    use super::RNodeObject;
+    use crate::rnodes::rnode::{EnumNodeType, RNode};
+    use crate::rnodes::rnode_double::RNodeDouble;
+    use std::collections::BTreeMap;
+    use std::rc::Rc;
+
+    #[test]
+    fn create_object_from_builder()
+    {
+        let key0 = String::from("key0");
+        let key1 = String::from("key1");
+        let val0 = Rc::new(RNodeDouble::new(123.45));
+        let val1 = Rc::new(RNodeDouble::new(-123.45));
+
+        let mut obj = RNodeObject::new(BTreeMap::<String, Rc<dyn RNode>>::new());
+        obj = obj.add_copy(&key0, val0.clone()).add_copy(&key1, val1.clone());
+        assert_eq!(obj.get_node_type(), EnumNodeType::OBJECT);
+
+        let opt_val0 = obj.get(&key0);
+        assert!(opt_val0.is_some());
+        let node_double = opt_val0.unwrap().downcast_rc::<RNodeDouble>().map_err(|_| "Shouldn't happen").unwrap();
+        assert_eq!(node_double.value, val0.value);
+
+        let opt_val1 = obj.get(&key1);
+        assert!(opt_val1.is_some());
+        let node_double = opt_val1.unwrap().downcast_rc::<RNodeDouble>().map_err(|_| "Shouldn't happen").unwrap();
+        assert_eq!(node_double.value, val1.value);
     }
 }
 
